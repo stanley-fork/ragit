d89ccddf540b6419aeb06dc5b8813fc0a4fab0f0
baehyunsol <baehyunsol@naver.com>
2024-10-26 10:25:34 +0900

    fix `normalize_image`
    1. It removes a tmp file.
    2. It doesn't generate a tmp file if unnecessary.

--- src/index/file/image.rs
+++ src/index/file/image.rs
@@ -1,6 +1,6 @@
 use crate::error::Error;
 use ragit_api::ImageType;
-use ragit_fs::read_bytes;
+use ragit_fs::{read_bytes, remove_file};
 
 pub fn normalize_image(bytes: Vec<u8>, image_type: ImageType) -> Result<Vec<u8>, Error> {
     let mut dynamic_image = image::load_from_memory_with_format(
@@ -12,7 +12,15 @@ pub fn normalize_image(bytes: Vec<u8>, image_type: ImageType) -> Result<Vec<u8>,
         dynamic_image = dynamic_image.resize(1024, 1024, image::imageops::FilterType::Triangle);
     }
 
+    // no modification at all
+    else if image_type == ImageType::Png {
+        return Ok(bytes);
+    }
+
     // TODO: I don't want save it to a tmp file. I want a direct `Vec<u8>`
     dynamic_image.save_with_format("._tmp.png", image::ImageFormat::Png)?;
-    Ok(read_bytes("._tmp.png")?)
+    let bytes = read_bytes("._tmp.png")?;
+    remove_file("._tmp.png")?;
+
+    Ok(bytes)
 }
