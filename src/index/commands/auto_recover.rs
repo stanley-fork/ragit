31811b99e36097915cda691a6538878684090ed5
baehyunsol <baehyunsol@naver.com>
2024-11-01 22:29:24 +0900

    fix `auto recover`
    Now all the tests are passing.

--- src/index/commands/auto_recover.rs
+++ src/index/commands/auto_recover.rs
@@ -13,6 +13,8 @@ impl Index {
 
         // It's re-created from scratch
         let mut chunk_index_map = HashMap::new();
+        let mut chunk_files = HashMap::new();
+        let mut chunk_count = 0;
 
         // It removes unused images
         let mut images = HashSet::new();
@@ -21,6 +23,7 @@ impl Index {
             match chunk::load_from_file(&chunk_file) {
                 Ok(chunks) => {
                     let mut new_chunks = Vec::with_capacity(chunks.len());
+                    let chunk_file_name = file_name(&chunk_file)?;
 
                     for chunk in chunks.into_iter() {
                         if let Some(file) = &curr_processing_file {
@@ -29,13 +32,19 @@ impl Index {
                             }
                         }
 
-                        chunk_index_map.insert(chunk.uid.clone(), file_name(&chunk_file)?);
+                        chunk_index_map.insert(chunk.uid.clone(), chunk_file_name.clone());
+
+                        match chunk_files.get_mut(&chunk_file_name) {
+                            Some(n) => { *n += 1; },
+                            None => { chunk_files.insert(chunk_file_name.clone(), 1); },
+                        }
 
                         for image in chunk.images.iter() {
                             images.insert(image.to_string());
                         }
 
                         new_chunks.push(chunk);
+                        chunk_count += 1;
                     }
 
                     // It also re-creates tfidf indexes
@@ -67,6 +76,17 @@ impl Index {
             if !images.contains(&file_name(&image_file)?) {
                 remove_file(&image_file)?;
             }
+        }
+
+        self.chunk_files = chunk_files;
+        self.chunk_count = chunk_count;
+
+        if self.chunk_count == 0 {
+            self.create_new_chunk_file()?;
+        }
+
+        if let Some(curr_processing_file) = curr_processing_file {
+            self.staged_files.push(curr_processing_file);
         }
 
         Ok(())
