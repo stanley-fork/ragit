f40d2c7c184f8318a73601497657cda5ed9e1d0a
baehyunsol <baehyunsol@naver.com>
2024-11-03 22:12:17 +0900

    improve output of `tests.py all`
    It dumps a json file containing result of all the tests.

--- tests/tests.py
+++ tests/tests.py
@@ -4,8 +4,9 @@ from cargo_tests import cargo_tests
 from end_to_end import end_to_end
 from external_bases import external_bases
 from images import images
+import os
 import sys
-from utils import clean
+from utils import clean, goto_root
 
 help_message = """
 Commands
@@ -21,15 +22,19 @@ Commands
 
     cargo_tests                 run `cargo test` on all the crates
 
-    all [model=dummy]           run all tests
+    all [model]                 run all tests
+                                It runs `end_to_end` twice. Once with `model = dummy`,
+                                and one more with the given model.
+                                It dumps the test result to `tests/results.json`.
 """
 
 if __name__ == "__main__":
     command = sys.argv[1] if len(sys.argv) > 1 else None
-    test_model = sys.argv[2] if len(sys.argv) > 2 else "dummy"
+    test_model = sys.argv[2] if len(sys.argv) > 2 else None
 
     try:
         if command == "end_to_end":
+            test_model = test_model or "dummy"
             end_to_end(test_model=test_model)
 
         elif command == "external_bases":
@@ -48,12 +53,61 @@ if __name__ == "__main__":
             cargo_tests()
 
         elif command == "all":
-            end_to_end(test_model=test_model)
-            external_bases()
-            add_and_rm()
-            auto_recover()
-            images()
-            cargo_tests()
+            import json
+            import time
+            import traceback
+
+            if test_model is None:
+                print("Please specify which model to run the tests with.")
+                sys.exit(1)
+
+            has_error = False
+            results = {}
+            tests = [
+                ("end_to_end_dummy", lambda: end_to_end(test_model="dummy")),
+                ("end_to_end_real", lambda: end_to_end(test_model=test_model)),
+                ("external_bases", external_bases),
+                ("add_and_rm", add_and_rm),
+                ("auto_recover", auto_recover),
+                ("images", images),
+                ("cargo_tests", cargo_tests),
+            ]
+
+            for name, test in tests:
+                try:
+                    start = time.time()
+                    test()
+
+                except Exception as e:
+                    has_error = True
+                    end = time.time()
+                    results[name] = {
+                        "pass": False,
+                        "error": str(e) + "\n" + traceback.format_exc(),
+                        "elapsed_ms": int((end - start) * 1000),
+                    }
+
+                else:
+                    end = time.time()
+                    results[name] = {
+                        "pass": True,
+                        "error": None,
+                        "elapsed_ms": int((end - start) * 1000),
+                    }
+
+                finally:
+                    clean()
+
+            goto_root()
+            os.chdir("tests")
+            result = json.dumps(results, indent=4)
+            print(result)
+
+            with open("results.json", "w") as f:
+                f.write(result)
+
+            if has_error:
+                sys.exit(1)
 
         else:
             print(help_message)
