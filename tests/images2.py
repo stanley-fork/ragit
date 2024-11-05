28ef33ac8ca2945805d192dfc1d1978c5dec6490
baehyunsol <baehyunsol@naver.com>
2024-11-05 22:42:59 +0900

    fix `images2.py`
    I forgot to add `sample2.md`. Now the test is passing.

--- tests/images2.py
+++ tests/images2.py
@@ -14,7 +14,7 @@ def images2(test_model: str):
     cargo_run(["config", "--set", "model", test_model])
     cargo_run(["config", "--set", "strict_file_reader", "true"])
     cargo_run(["config", "--set", "dump_log", "true"])
-    cargo_run(["add", "sample.md"])
+    cargo_run(["add", "sample.md", "sample2.md"])
     cargo_run(["check"])
     cargo_run(["build"])
     os.chdir(".rag_index/images")
