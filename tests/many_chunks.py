d24c655b7e4188e554a44ceb20fd8cf0a2cd9e19
baehyunsol <baehyunsol@naver.com>
2024-11-12 20:54:59 +0900

    refactor tests
    Removed `interrupts.py` and moved its test cases to `many_chunks.py`.

--- tests/many_chunks.py
+++ tests/many_chunks.py
@@ -1,5 +1,6 @@
-from utils import cargo_run, goto_root, mk_and_cd_tmp_dir, rand_word, write_string
 from random import randint, seed as rand_seed, shuffle
+from subprocess import TimeoutExpired
+from utils import cargo_run, goto_root, mk_and_cd_tmp_dir, rand_word, write_string
 
 def many_chunks():
     rand_seed(0)
@@ -8,19 +9,46 @@ def many_chunks():
     txt_files = []
     tfidf_map = []
 
-    for i in range(randint(1000, 2000)):
+    for i in range(2000):
         file_name = f"{i:04}.txt"
         txt_files.append(file_name)
-        words = [rand_word() for _ in range(randint(3, 8))]
+
+        if randint(0, 3) == 0:
+            words = [rand_word() for _ in range(randint(500, 1000))]
+
+        else:
+            words = [rand_word() for _ in range(randint(5, 10))]
+
         write_string(file_name, " ".join(words))
         tfidf_map.append((words[0], file_name))
 
     cargo_run(["init"])
     cargo_run(["config", "--set", "model", "dummy"])
+    cargo_run(["config", "--set", "sleep_after_llm_call", "10"])
+    cargo_run(["config", "--set", "chunk_size", "4000"])
     cargo_run(["add", *txt_files])
     cargo_run(["check"])
-    cargo_run(["build"])
-    cargo_run(["check"])
+    break2 = False
+
+    while True:
+        # there are 2 cases to cover:
+        # 1. implicit --auto-recover invoked by `rag build`
+        # 2. explicit `rag check --auto-recover`
+        cargo_run(["check", "--auto-recover"])
+
+        for _ in range(4):
+            try:
+                cargo_run(["build"], timeout=5.0)
+
+            except TimeoutExpired:
+                pass
+
+            else:
+                break2 = True
+                break
+
+        if break2:
+            break
 
     cargo_run(["remove", "0010.txt"])
     cargo_run(["check"])
@@ -30,6 +58,7 @@ def many_chunks():
     cargo_run(["check"])
 
     # it takes too long to run tfidf thousands of times
+    # TODO: run tfidf thousands times when K-V DB is implemented
     shuffle(tfidf_map)
     tfidf_map = tfidf_map[:20]
 
