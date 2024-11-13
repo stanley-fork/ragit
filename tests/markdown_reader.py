69513b38b1a62488294a293bc8fc46fa36753048
baehyunsol <baehyunsol@naver.com>
2024-11-13 21:04:38 +0900

    add more markdown tests
    Added 2 samples to `markdown_reader.py`. It's not fixed yet.

--- tests/markdown_reader.py
+++ tests/markdown_reader.py
@@ -6,6 +6,19 @@ this is a sentence. `this is an unterminated
 code span`.
 '''
 
+# not fixed yet (rustc-dev-guide/diagnostics.md)
+sample2 = "\n".join([
+    "a" * 3500,
+    *(["aaaa"] * 200),
+])
+
+# not fixed yet
+sample3 = "\n".join([
+    "a" * 6000,
+    "aa",
+    "aa",
+])
+
 def markdown_reader():
     goto_root()
     mk_and_cd_tmp_dir()
@@ -15,5 +28,19 @@ def markdown_reader():
 
     write_string("sample1.md", sample1)
     cargo_run(["add", "sample1.md"])
+    cargo_run(["build"], timeout=20.0)
+    cargo_run(["check"])
+
+    write_string("sample2.md", sample2)
+    cargo_run(["config", "--set", "chunk_size", "4000"])
+    cargo_run(["config", "--set", "slide_len", "1000"])
+    cargo_run(["add", "sample2.md"])
+    cargo_run(["build"], timeout=20.0)
+    cargo_run(["check"])
+
+    write_string("sample3.md", sample3)
+    cargo_run(["config", "--set", "chunk_size", "4000"])
+    cargo_run(["config", "--set", "slide_len", "1000"])
+    cargo_run(["add", "sample3.md"])
     cargo_run(["build"], timeout=20.0)
     cargo_run(["check"])
