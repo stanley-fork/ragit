715b5ebbbf5bf1e071adeb0f79d6ba4708aebebb
baehyunsol <baehyunsol@naver.com>
2024-12-30 19:50:40 +0900

    bump git commit hash

--- tests/migrate.py
+++ tests/migrate.py
@@ -8,7 +8,7 @@ from utils import cargo_run, goto_root, mk_and_cd_tmp_dir, write_string
 def checkout(version: str):
     commit_hashes = {
         "0.1.1": "a168d13af967",
-        "0.2.0": "d6030d107105",
+        "0.2.0": "a183c071d068",
     }
 
     try:
