89f5bc33409084d03076443838f5d3ceeba2e1d8
baehyunsol <baehyunsol@naver.com>
2025-01-09 22:15:32 +0900

    add test for ragignore
    It's failing, tho.

--- src/index/commands/add.rs
+++ src/index/commands/add.rs
@@ -243,13 +243,7 @@ impl Ignore {
                 continue;
             }
 
-            let mut t = t.to_string();
-
-            if !t.starts_with("/") {
-                t = format!("**/{t}");
-            }
-
-            patterns.push(IgnorePattern::parse(&t));
+            patterns.push(IgnorePattern::parse(t));
         }
 
         Ignore { patterns }
@@ -294,6 +288,7 @@ impl Ignore {
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct IgnorePattern {
     // TODO: I need a smarter implementation
     r: Regex,
@@ -302,6 +297,12 @@ pub struct IgnorePattern {
 impl IgnorePattern {
     // TODO: I need a smarter implementation
     pub fn parse(pattern: &str) -> Self {
+        let mut pattern = pattern.to_string();
+
+        if !pattern.starts_with("/") {
+            pattern = format!("**/{pattern}");
+        }
+
         let replaces = vec![
             (r"^\*\*$", r".+"),
             (r"^\*\*/", r"([^/]+/)_ast"),
@@ -329,12 +330,19 @@ impl IgnorePattern {
         pattern = pattern.replace("_dot", "\\.");
         pattern = pattern.replace("_pls", "\\+");
         pattern = pattern.replace("_und", "_");
-        IgnorePattern { r: Regex::new(&pattern).unwrap() }
+        IgnorePattern { r: Regex::new(&format!("^{pattern}$")).unwrap() }
     }
 
     // `path` must be a normalized, relative path
     pub fn is_match(&self, path: &str) -> bool {
-        self.r.is_match(path)
+        let mut path = path.to_string();
+
+        // there's no reason to treat `a/b` and `a/b/` differently
+        if path.len() > 1 && path.ends_with("/") {
+            path = path.get(0..(path.len() - 1)).unwrap().to_string();
+        }
+
+        self.r.is_match(&path)
     }
 }
 
@@ -342,8 +350,10 @@ impl IgnorePattern {
 mod tests {
     use super::IgnorePattern;
 
+    // TODO: find a correct gitignore-parser and make sure that these samples are correct
     #[test]
     fn ignore_test() {
+        // (pattern, matched, not_matched)
         let sample = vec![
             (
                 "abc",
@@ -353,25 +363,90 @@ mod tests {
                     "a/abc",
                 ],
                 vec![
-                    // I'm not sure about gitignore, but ragignore will never allow this
-                    "/abc",
                     "abc/a",
                     "ab",
+
+                    // TODO
+                    //   1. I'm not sure whether gitignore allows this or not.
+                    //   2. `Index::get_rel_path` will never generate a path that starts with "/".
+                    //   3. Then, what should we do with this case?
+                    "/abc",
+                ],
+            ), (
+                "ab*",
+                vec![
+                    "a/ab",
+                    "a/abc",
+                    "ab",
+                    "abc",
+                ],
+                vec![
+                    "dab",
+                    "aab",
+                ],
+            ), (
+                "a/**/b",
+                vec![
+                    "a/b", "a/b/",
+                    "a/b/b", "a/b/b/",
+                    "a/c/b", "a/c/b/",
+                    "a/d/d/d/b", "a/d/d/d/b/",
+                    "c/a/b", "c/a/b/",
+                    "c/a/b/b", "c/a/b/b/",
+                    "c/a/c/b", "c/a/c/b/",
+                    "c/a/d/d/d/b", "c/a/d/d/d/b/",
+                ],
+                vec![
+                    "b/a",
+                    "a/a",
+                    "b/b",
+                ],
+            ), (
+                "*.json",
+                vec![
+                    "a.json",
+                    "res/a.json",
+                ],
+                vec![],
+            ), (
+                "/*.json",
+                vec![
+                    "a.json",
+                ],
+                vec![
+                    "a/b.json",
                 ],
             ),
-            // TODO: more cases
         ];
+        let mut failures = vec![];
 
-        for (pattern, matched, not_matched) in sample {
-            let pattern = IgnorePattern::parse(pattern);
+        for (pattern_str, matched, not_matched) in sample {
+            let pattern = IgnorePattern::parse(pattern_str);
 
             for path in matched {
-                assert!(pattern.is_match(path));
+                if !pattern.is_match(path) {
+                    failures.push((true, pattern_str.to_string(), pattern.clone(), path.to_string()));
+                }
             }
 
             for path in not_matched {
-                assert!(!pattern.is_match(path));
+                if pattern.is_match(path) {
+                    failures.push((false, pattern_str.to_string(), pattern.clone(), path.to_string()));
+                }
             }
+        }
+
+        if !failures.is_empty() {
+            panic!(
+                "{}",
+                failures.iter().map(
+                    |(has_to_match, pattern_str, pattern, path)| format!(
+                        "(pattern: {pattern_str:?} -> {pattern:?}, path: {path:?}) {}, but {}",
+                        if *has_to_match { "has to match" } else { "must not match" },
+                        if *has_to_match { "but doesn't" } else { "but does" },
+                    )
+                ).collect::<Vec<_>>().join("\n"),
+            );
         }
     }
 }
