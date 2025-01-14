04c98df9bf8ec1c33e282131cb9d3d745d393559
baehyunsol <baehyunsol@naver.com>
2025-01-14 20:52:27 +0900

    refactor `tests/cargo_tests.py`
    Now it runs all the tests even when some tests fail.

--- tests/cargo_tests.py
+++ tests/cargo_tests.py
@@ -3,20 +3,43 @@ import subprocess
 from subprocess import CalledProcessError
 from utils import goto_root
 
+# returns error messages, if exists
+def run_cargo_test() -> list[str]:
+    errors = []
+
+    for action in [
+        ["cargo", "test"],
+        ["cargo", "test", "--release"],
+    ]:
+        result = subprocess.run(action, capture_output=True, text=True)
+
+        if result.returncode != 0:
+            errors.append(f"""
+#####################
+### path: command ###
+{os.getcwd()}: {' '.join(action)}
+
+### status_code ###
+{result.returncode}
+
+### stdout ###
+{result.stdout}
+
+### stderr ###
+{result.stderr}
+""")
+
+    return errors
+
 def cargo_tests():
     goto_root()
-    subprocess.run(["cargo", "test"], check=True)
-    subprocess.run(["cargo", "test", "--release"], check=True)
+    errors = run_cargo_test()
     os.chdir("crates")
 
     for crate in ["api", "fs", "korean", "pdl", "server"]:
         os.chdir(crate)
-
-        try:
-            subprocess.run(["cargo", "test"], check=True)
-            subprocess.run(["cargo", "test", "--release"], check=True)
-
-        except CalledProcessError as e:
-            raise Exception(f"Error in {crate}: {e}")
-
+        errors += run_cargo_test()
         os.chdir("..")
+
+    if len(errors) > 0:
+        raise Exception("\n\n".join(errors))
