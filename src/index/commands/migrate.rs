stanley-fork/ragit

commit 470ab07dacb6c535eb97d1e4dc1214ae22bb561e

draft archive 2
I fixed some bugs and added a test suite for the new commands. But there
are still bugs and rough edges.

src/index/commands/migrate.rs
@@ -24,6 +24,7 @@ use ragit_fs::{
     remove_file,
     rename,
     set_extension,
+    try_create_dir,
     write_bytes,
     write_string,
 };
@@ -316,7 +317,7 @@ fn migrate_0_1_1_to_0_2_x(root_dir: &Path) -> Result<(), Error> {
         )?;
 
         if !exists(&image_at) {
-            create_dir_all(&image_at)?;
+            try_create_dir(&image_at)?;
         }
 
         copy_file(
@@ -464,7 +465,7 @@ fn migrate_0_1_1_to_0_2_x(root_dir: &Path) -> Result<(), Error> {
                                         )?;
 
                                         if !exists(&chunk_at) {
-                                            create_dir_all(&chunk_at)?;
+                                            try_create_dir(&chunk_at)?;
                                         }
 
                                         match file_to_chunks_map.get_mut(&file_name) {
@@ -553,7 +554,7 @@ fn migrate_0_1_1_to_0_2_x(root_dir: &Path) -> Result<(), Error> {
         let index_path = join(&file_index_at, file_uid.get(0..2).unwrap())?;
 
         if !exists(&index_path) {
-            create_dir_all(&index_path)?;
+            try_create_dir(&index_path)?;
         }
 
         write_string(

crates/fs/src/lib.rs
@@ -233,6 +233,17 @@ pub fn parent(path: &str) -> Result<String, FileError> {
     )
 }
 
+/// It's like `create_dir` but does not raise an error if `path` already exists
+pub fn try_create_dir(path: &str) -> Result<(), FileError> {
+    match fs::create_dir(path) {
+        Ok(()) => Ok(()),
+        Err(e) => match e.kind() {
+            io::ErrorKind::AlreadyExists => Ok(()),
+            _ => Err(FileError::from_std(e, path)),
+        },
+    }
+}
+
 pub fn create_dir(path: &str) -> Result<(), FileError> {
     fs::create_dir(path).map_err(|e| FileError::from_std(e, path))
 }

src/chunk.rs
@@ -11,12 +11,12 @@ use ragit_api::{
 };
 use ragit_fs::{
     WriteMode,
-    create_dir_all,
     exists,
     normalize,
     parent,
     read_bytes,
     set_extension,
+    try_create_dir,
     write_bytes,
 };
 use ragit_pdl::{
@@ -87,7 +87,6 @@ pub fn load_from_file(path: &str) -> Result<Chunk, Error> {
     }
 }
 
-/// It also creates a tfidf index of the chunk.
 pub fn save_to_file(
     path: &str,
     chunk: &Chunk,
@@ -96,20 +95,23 @@ pub fn save_to_file(
     compression_threshold: u64,
     compression_level: u32,
     root_dir: &str,
+    create_tfidf: bool,
 ) -> Result<(), Error> {
     let mut result = serde_json::to_vec_pretty(chunk)?;
     let tfidf_path = set_extension(path, "tfidf")?;
     let parent_path = parent(path)?;
 
     if !exists(&parent_path) {
-        create_dir_all(&parent_path)?;
+        try_create_dir(&parent_path)?;
     }
 
-    tfidf::save_to_file(
-        &tfidf_path,
-        chunk,
-        root_dir,
-    )?;
+    if create_tfidf {
+        tfidf::save_to_file(
+            &tfidf_path,
+            chunk,
+            root_dir,
+        )?;
+    }
 
     if result.len() as u64 > compression_threshold {
         let mut compressed = vec![];

src/index.rs
@@ -27,6 +27,7 @@ use ragit_fs::{
     read_string,
     remove_file,
     set_extension,
+    try_create_dir,
     write_bytes,
     write_string,
 };
@@ -415,7 +416,7 @@ impl Index {
         let parent_path = parent(&image_path)?;
 
         if !exists(&parent_path) {
-            create_dir_all(&parent_path)?;
+            try_create_dir(&parent_path)?;
         }
 
         let image_bytes = read_bytes(&image_path)?;
@@ -831,7 +832,7 @@ impl Index {
         let parent_path = parent(&file_index_path)?;
 
         if !exists(&parent_path) {
-            create_dir_all(&parent_path)?;
+            try_create_dir(&parent_path)?;
         }
 
         uid::save_to_file(&file_index_path, uids, UidWriteMode::Naive)

src/index/commands/archive/create.rs
@@ -177,6 +177,15 @@ impl Index {
             }
         }
 
+        if !curr_image_block.is_empty() {
+            workers[round_robin % workers.len()].send(Request::Compress(BlockType::ImageBytes, curr_image_block)).map_err(|_| Error::MPSCError(String::from("Create-archive worker hung up.")))?;
+            round_robin += 1;
+        }
+
+        if !curr_image_desc_block.is_empty() {
+            workers[round_robin % workers.len()].send(Request::Compress(BlockType::ImageDesc, curr_image_desc_block)).map_err(|_| Error::MPSCError(String::from("Create-archive worker hung up.")))?;
+        }
+
         let mut curr_output_size = 0;
         let mut curr_output_seq = 0;
         let mut killed_workers = vec![];
@@ -205,7 +214,7 @@ impl Index {
                         Response::Compressed(block_type, block_path) => {
                             let block_size = file_size(&block_path)?;
 
-                            if block_size + curr_output_size > size_limit_comp {
+                            if block_size + curr_output_size > size_limit_comp && curr_output_size > 0 {
                                 curr_output_size = block_size;
                                 curr_output_seq += 1;
                                 curr_output_file = format!("{output}-{curr_output_seq:04}");

src/index/commands/archive/extract.rs
@@ -8,20 +8,19 @@ use crate::index::{
     IMAGE_DIR_NAME,
     Index,
     INDEX_FILE_NAME,
+    LoadMode,
     METADATA_FILE_NAME,
 };
 use crate::uid::Uid;
 use ragit_fs::{
     WriteMode,
-    create_dir,
-    create_dir_all,
     exists,
     file_size,
-    join,
     join3,
     parent,
     read_bytes_offset,
-    remove_dir,
+    remove_dir_all,
+    try_create_dir,
     write_bytes,
 };
 use ragit_pdl::decode_base64;
@@ -42,22 +41,9 @@ impl Index {
         }
 
         let workers = init_workers(workers, root_dir);
-        create_dir_all(&join(
-            root_dir,
-            INDEX_DIR_NAME,
-        )?)?;
-        create_dir(&join3(
-            root_dir,
-            INDEX_DIR_NAME,
-            CHUNK_DIR_NAME,
-        )?)?;
-        create_dir(&join3(
-            root_dir,
-            INDEX_DIR_NAME,
-            IMAGE_DIR_NAME,
-        )?)?;
 
         match Index::extract_archive_worker(
+            root_dir,
             archives,
             &workers,
         ) {
@@ -68,7 +54,7 @@ impl Index {
                 }
 
                 if exists(root_dir) {
-                    remove_dir(root_dir)?;
+                    remove_dir_all(root_dir)?;
                 }
 
                 Err(e)
@@ -77,10 +63,12 @@ impl Index {
     }
 
     fn extract_archive_worker(
+        root_dir: &str,
         archives: Vec<String>,
         workers: &[Channel],
     ) -> Result<(), Error> {
         let mut round_robin = 0;
+        Index::new(root_dir.to_string())?;
 
         for archive in archives.iter() {
             let archive_size = file_size(archive)?;
@@ -147,6 +135,10 @@ impl Index {
             thread::sleep(Duration::from_millis(100));
         }
 
+        // it creates file indexes and tfidfs
+        let mut index = Index::load(root_dir.to_string(), LoadMode::Minimum)?;
+        index.recover()?;
+
         Ok(())
     }
 }
@@ -185,7 +177,7 @@ fn event_loop(
                                 INDEX_FILE_NAME,
                             )?,
                             &index,
-                            WriteMode::AlwaysCreate,
+                            WriteMode::CreateOrTruncate,
                         )?;
                     },
                     BlockType::Chunk => {
@@ -200,7 +192,7 @@ fn event_loop(
                             )?;
 
                             if !exists(&parent(&chunk_at)?) {
-                                create_dir(&parent(&chunk_at)?)?;
+                                try_create_dir(&parent(&chunk_at)?)?;
                             }
 
                             chunk::save_to_file(
@@ -209,6 +201,7 @@ fn event_loop(
                                 0,
                                 3,
                                 &root_dir,
+                                false,  // create tfidf
                             )?;
                         }
                     },
@@ -226,7 +219,7 @@ fn event_loop(
                             )?;
 
                             if !exists(&parent(&image_at)?) {
-                                create_dir(&parent(&image_at)?)?;
+                                try_create_dir(&parent(&image_at)?)?;
                             }
 
                             write_bytes(
@@ -249,7 +242,7 @@ fn event_loop(
                             )?;
 
                             if !exists(&parent(&desc_at)?) {
-                                create_dir(&parent(&desc_at)?)?;
+                                try_create_dir(&parent(&desc_at)?)?;
                             }
 
                             write_bytes(
@@ -268,7 +261,7 @@ fn event_loop(
                                 METADATA_FILE_NAME,
                             )?,
                             &serde_json::to_vec_pretty(&meta)?,
-                            WriteMode::AlwaysCreate,
+                            WriteMode::CreateOrTruncate,
                         )?;
                     },
                     _ => todo!(),

src/index/commands/build.rs
@@ -12,9 +12,9 @@ use crate::uid::Uid;
 use ragit_api::record::Record;
 use ragit_fs::{
     WriteMode,
-    create_dir_all,
     exists,
     parent,
+    try_create_dir,
     write_bytes,
 };
 use sha3::{Digest, Sha3_256};
@@ -372,7 +372,7 @@ async fn event_loop(
                         let parent_path = parent(&image_path)?;
     
                         if !exists(&parent_path) {
-                            create_dir_all(&parent_path)?;
+                            try_create_dir(&parent_path)?;
                         }
     
                         write_bytes(
@@ -389,6 +389,7 @@ async fn event_loop(
                         index.build_config.compression_threshold,
                         index.build_config.compression_level,
                         &index.root_dir,
+                        true,  // create tfidf
                     )?;
                     tx_to_main.send(Response::ChunkComplete {
                         file: file.clone(),

src/index/commands/clone.rs
@@ -7,12 +7,12 @@ use crate::uid::Uid;
 use ragit_fs::{
     WriteMode,
     create_dir,
-    create_dir_all,
     exists,
     join3,
     parent,
     remove_dir_all,
     set_extension,
+    try_create_dir,
     write_bytes,
 };
 use reqwest::Url;
@@ -108,7 +108,7 @@ impl Index {
                 let parent = parent(&image_path)?;
 
                 if !exists(&parent) {
-                    create_dir_all(&parent)?;
+                    try_create_dir(&parent)?;
                 }
 
                 write_bytes(
@@ -168,7 +168,7 @@ impl Index {
             let parent = parent(&chunk_path)?;
 
             if !exists(&parent) {
-                create_dir_all(&parent)?;
+                try_create_dir(&parent)?;
             }
 
             write_bytes(
@@ -184,6 +184,7 @@ impl Index {
                 0,
                 9,
                 &index.root_dir,
+                true,  // create tfidf
             )?;
         }
 

commit 696a058677f2f585cc3e184e3a432befd04c9fd1

fix `migrate`
I guess replacing `create_dir_all` with `try_create_dir` at 470ab07d
introduced bugs in `migrate`.

src/index/commands/migrate.rs
@@ -293,6 +293,10 @@ fn migrate_0_1_1_to_0_2_x(root_dir: &Path) -> Result<(), Error> {
     )?;
     let mut image_size_map = HashMap::new();
 
+    if !exists(&image_dir) {
+        create_dir(&image_dir)?;
+    }
+
     for image_file in read_dir(&image_dir, false)? {
         let curr_ext = extension(&image_file)?.unwrap_or(String::new());
 
@@ -547,6 +551,10 @@ fn migrate_0_1_1_to_0_2_x(root_dir: &Path) -> Result<(), Error> {
         ".ragit",
         "files",
     )?;
+
+    if !exists(&file_index_at) {
+        create_dir(&file_index_at)?;
+    }
 
     for (file_name, mut chunks) in file_to_chunks_map.into_iter() {
         chunks.sort_by_key(|(_, index)| *index);