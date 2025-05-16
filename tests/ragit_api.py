0001f366ab54d003b135616c5759dc16ddc47529
baehyunsol <baehyunsol@naver.com>
2025-05-16 23:27:05 +0900

    fix `tests/ragit_api.py`
    Previous commit removed binary of `crates/api/` and instead created `rag
    pdl` command. `tests/ragit_api.py` was using the binary and now instead
    using the `rag pdl` command.

--- tests/ragit_api.py
+++ tests/ragit_api.py
@@ -1,14 +1,15 @@
 import os
 import subprocess
-from utils import goto_root, write_string
+from utils import (
+    cargo_run,
+    goto_root,
+    mk_and_cd_tmp_dir,
+    write_string,
+)
 
 def ragit_api(test_model: str):
     goto_root()
-    os.chdir("crates/api")
+    mk_and_cd_tmp_dir()
     write_string("hello_world.pdl", "\n<|user|>\n\nWhat's your name?\n")
-
-    try:
-        subprocess.run(["cargo", "run", "--release", '--', '--model', test_model, '--input', 'hello_world.pdl'], check=True)
-
-    finally:
-        os.remove("hello_world.pdl")
+    cargo_run(["init"])
+    cargo_run(["pdl", "hello_world.pdl", "--model", test_model])
