b02955c6bdd1ff8da9deb51165c5740a377b4bfd
baehyunsol <baehyunsol@naver.com>
2025-05-26 21:45:25 +0900

    fix `tests/real_repos.py`
    I added metadata to the repositories. It's another step for ragithub.

--- tests/real_repos.py
+++ tests/real_repos.py
@@ -2,7 +2,115 @@ import os
 import re
 import shutil
 import subprocess
-from utils import cargo_run, goto_root, ls_recursive, mk_and_cd_tmp_dir
+from utils import (
+    cargo_run,
+    count_chunks,
+    count_files,
+    get_commit_hash,
+    goto_root,
+    ls_recursive,
+    mk_and_cd_tmp_dir,
+)
+
+repositories = [
+    {
+        "git-name": "git",
+        "description": "fast, scalable, distributed revision control system",
+        "git-url": "https://github.com/git/git",
+        "ragit-name": "git",
+        "extensions": ["adoc"],
+    }, {
+        "git-name": "postgresql",
+        "description": "The World's Most Advanced Open Source Relational Database",
+        "git-url": "https://git.postgresql.org/git/postgresql.git",
+        "ragit-name": "postgresql",
+        "extensions": ["sgml"],
+    }, {
+        "git-name": "rustc-dev-guide",
+        "description": "A guide to how rustc works and how to contribute to it.",
+        "git-url": "https://github.com/rust-lang/rustc-dev-guide/",
+        "ragit-name": "rustc",
+        "extensions": ["md"],
+    }, {
+        "git-name": "docs",
+        "description": "Docker helps developers bring their ideas to life by conquering the complexity of app development.",
+        "git-url": "https://github.com/docker/docs/",
+        "ragit-name": "docker",
+        "extensions": ["md"],
+    }, {
+        "git-name": "website",
+        "description": "Production-Grade Container Scheduling and Management",
+        "git-url": "https://github.com/kubernetes/website",
+        "ragit-name": "kubernetes",
+        "extensions": ["md"],
+        "rm-r": [
+            "content/bn",
+            "content/de",
+            "content/es",
+            "content/fr",
+            "content/hi",
+            "content/id",
+            "content/it",
+            "content/ja",
+            "content/ko",
+            "content/pl",
+            "content/pt-br",
+            "content/ru",
+            "content/uk",
+            "content/vi",
+            "content/zh-cn",
+        ],
+    }, {
+        "git-name": "tera",
+        "description": "A template engine for Rust based on Jinja2/Django",
+        "git-url": "https://github.com/Keats/tera",
+        "ragit-name": "tera",
+        "extensions": ["md"],
+    }, {
+        "git-name": "neovim",
+        "description": "Vim-fork focused on extensibility and usability",
+        "git-url": "https://github.com/neovim/neovim",
+        "ragit-name": "neovim",
+        "extensions": ["txt"],
+
+        # contains large and meaningless text files for tests
+        "rm-r": ["test"],
+    }, {
+        "git-name": "nushell.github.io",
+        "description": "A new type of shell",
+        "git-url": "https://github.com/nushell/nushell.github.io",
+        "ragit-name": "nushell",
+        "extensions": ["md"],
+        "rm-r": [
+            "de",
+            "es",
+            "fr",
+            "ja",
+            "pt-BR",
+            "ru",
+            "tr",
+            "zh-CN",
+        ],
+    }, {
+        "git-name": "nix",
+        "description": "Nix, the purely functional package manager",
+        "git-url": "https://github.com/NixOS/nix",
+        "ragit-name": "nix",
+        "extensions": ["md"],
+    }, {
+        "git-name": "nixpkgs",
+        "description": "Nix Packages collection & NixOS",
+        "git-url": "https://github.com/NixOS/nixpkgs",
+        "ragit-name": "nixpkgs",
+        "extensions": ["md"],
+    }, {
+        "git-name": "zed",
+        "description": "Code at the speed of thought – Zed is a high-performance, multiplayer code editor from the creators of Atom and Tree-sitter.",
+        "git-url": "https://github.com/zed-industries/zed",
+        "ragit-name": "zed",
+        "extensions": ["md"],
+    },
+]
 
 def real_repos(
     # If it's not set, it writes to `/samples/{repo}`, which might overwrite an existing knowledge-base.
@@ -30,31 +138,20 @@ def real_repos(
     if not os.path.exists("clone-here"):
         os.mkdir("clone-here")
 
-    for (url, (old_name, new_name), ext) in [
-        ("https://github.com/git/git", ("git", "git"), "adoc"),
-        ("https://git.postgresql.org/git/postgresql.git", ("postgresql", "postgresql"), "sgml"),
-        ("https://github.com/rust-lang/rustc-dev-guide/", ("rustc-dev-guide", "rustc"), "md"),
-        ("https://github.com/docker/docs/", ("docs", "docker"), "md"),
-        ("https://github.com/kubernetes/website", ("website", "kubernetes"), "md"),
-        ("https://github.com/Keats/tera", ("tera", "tera"), "md"),
-        ("https://github.com/neovim/neovim", ("neovim", "neovim"), "txt"),
-        ("https://github.com/nushell/nushell.github.io", ("nushell.github.io", "nushell"), "md"),
-        ("https://github.com/NixOS/nix", ("nix", "nix"), "md"),
-        ("https://github.com/NixOS/nixpkgs", ("nixpkgs", "nixpkgs"), "md"),
-        ("https://github.com/zed-industries/zed", ("zed", "zed"), "md"),
-    ]:
-        if repo != "all" and repo != new_name:
+    for r in repositories:
+        if repo != "all" and repo != r["ragit-name"]:
             continue
 
         os.chdir("clone-here")
 
-        if os.path.exists(old_name):
-            shutil.rmtree(old_name)
+        if os.path.exists(r["git-name"]):
+            shutil.rmtree(r["git-name"])
 
-        subprocess.run(["git", "clone", url, "--depth=1"], check=True)
-        new_path = os.path.join("..", new_name)
-        shutil.move(old_name, new_path)
+        subprocess.run(["git", "clone", r["git-url"], "--depth=1"], check=True)
+        new_path = os.path.join("..", r["ragit-name"])
+        shutil.move(r["git-name"], new_path)
         os.chdir(new_path)
+        git_hash = get_commit_hash()
         shutil.rmtree(".git")
 
         # `cargo_run` will get confused
@@ -64,10 +161,14 @@ def real_repos(
         cargo_run(["init"])
         cargo_run(["config", "--set", "model", test_model])
         cargo_run(["config", "--set", "strict_file_reader", "true"])
-        clean_up_repository(new_name)
+
+        for rm_r in r.get("rm-r", []):
+            shutil.rmtree(rm_r)
 
         # TODO: implement `rag add **/*.md` instead of relying on shell's glob patterns
-        cargo_run(["add", *ls_recursive(ext)])
+        for ext in r["extensions"]:
+            cargo_run(["add", *ls_recursive(ext)])
+
         cargo_run(["build"])
         cargo_run(["check"])
 
@@ -75,7 +176,7 @@ def real_repos(
         # ragit's fault or their fault.
         # This process cannot be automated. It automatically collects and dumps the error
         # messages but it will not affect the result of this test.
-        file_errors[new_name] = extract_error_messages(cargo_run(["build"], stdout=True))
+        file_errors[r["ragit-name"]] = extract_error_messages(cargo_run(["build"], stdout=True))
 
         # For testing purposes, `strict_file_reader=true` makes more sense. But I also
         # want to use this script to create real-world knowledge-bases, and for that,
@@ -83,6 +184,17 @@ def real_repos(
         cargo_run(["config", "--set", "strict_file_reader", "false"])
         cargo_run(["build"])
         cargo_run(["check"])
+
+        # It's included in readme.
+        cargo_run(["meta", "--set", "reproduce", how_to_reproduce(r, test_model)])
+        cargo_run(["meta", "--set", "git-hash", git_hash])
+        cargo_run(["meta", "--set", "git-url", r["git-url"]])
+        cargo_run(["meta", "--set", "description", r["description"]])
+        cargo_run(["meta", "--set", "ai-model", test_model])
+        cargo_run(["meta", "--set", "chunk-count", str(count_chunks())])
+        cargo_run(["meta", "--set", "file-count", str(count_files()[2])])
+
+        add_readme(r, test_model)
 
         os.chdir("..")
 
@@ -100,20 +212,6 @@ def real_repos(
         for error in errors:
             print(f"    {error}")
 
-def clean_up_repository(repo: str):
-    if repo == "kubernetes":
-        for lang in [
-            "bn", "de", "es", "fr",
-            "hi", "id", "it", "ja",
-            "ko", "pl", "pt-br", "ru",
-            "uk", "vi", "zh-cn",
-        ]:
-            shutil.rmtree(f"content/{lang}")
-
-    if repo == "neovim":
-        # contains large and meaningless text files for tests
-        shutil.rmtree("test")
-
 def extract_error_messages(stdout: str) -> list[str]:
     state = "i"
     errors = []
@@ -130,6 +228,51 @@ def extract_error_messages(stdout: str) -> list[str]:
                 errors.append(e)
 
     return errors
+
+def how_to_reproduce(repository, model: str) -> str:
+    rm_rs = "".join([f"\nrm -r {r};" for r in repository.get("rm-r", [])])
+    rag_adds = "\n".join([f"rag add **/*.{ext};" for ext in repository["extensions"]])
+
+    return f"""
+git clone {repository["git-url"]};
+cd {repository["git-name"]};{rm_rs}
+rag init;
+# set api key of your model
+rag config --set model {model};
+{rag_adds}
+rag build;
+"""
+
+def add_readme(repository, model: str):
+    from datetime import datetime
+    git_hash = cargo_run(["meta", "--get", "git-hash"], stdout=True).strip()
+    chunk_count = cargo_run(["meta", "--get", "chunk-count"], stdout=True).strip()
+    file_count = cargo_run(["meta", "--get", "file-count"], stdout=True).strip()
+    reproduce = cargo_run(["meta", "--get", "reproduce"], stdout=True).strip()
+
+    readme = f"""# {repository["ragit-name"]}
+
+{repository["description"]}
+
+This knowledge-base was auto-generated by script. It's built by {model} at {datetime.now()}.
+
+- source: [{repository["git-url"]}]({repository["git-url"]})
+- git hash: {git_hash}
+- chunks: {chunk_count}
+- files: {file_count}
+
+## How to clone
+
+`rag clone http://ragit.baehyunsol.com/sample/{repository["ragit-name"]}`
+
+## How to reproduce
+
+```sh
+{reproduce}
+```
+"""
+    cargo_run(["meta", "--set", "readme", readme])
+    return readme
 
 if __name__ == "__main__":
     import sys
