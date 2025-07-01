17fc58590a590716ebaeb74fa2b8da6389a9afdf
baehyunsol <baehyunsol@naver.com>
2025-07-01 23:12:48 +0900

    fix `tests/migrate2.py`
    0.3.x is not compatible with the newest version of ragit-server, so I
    modified the test code to make it run.

--- tests/migrate2.py
+++ tests/migrate2.py
@@ -6,40 +6,37 @@ from utils import cargo_run, goto_root, mk_and_cd_tmp_dir
 def migrate2():
     goto_root()
     mk_and_cd_tmp_dir()
-    errors = []
     curr_version = "0.4.2"
 
     # ragit 0.2.1 or older versions are not compatible with the current implementation of ragit-server
     for old_version in ["0.3.0", "0.3.5", "0.4.0"]:
         checkout(old_version)
 
-        for url in [
-            "https://ragit.baehyunsol.com/sample/ragit",
-            "https://ragit.baehyunsol.com/sample/git",
-            "https://ragit.baehyunsol.com/sample/rustc",
+        for base, url in [
+            ("ragit", "https://ragit.baehyunsol.com/sample/ragit"),
+            ("git", "https://ragit.baehyunsol.com/sample/git"),
+            ("rustc", "https://ragit.baehyunsol.com/sample/rustc"),
         ]:
-            clone_result = cargo_run(["clone", url], check=False, output_schema=["returncode", "stdout", "stderr"])
+            # ragit 0.3.x are not compatible with the current implementation of ragit-server, but I want to test them anyway
+            if old_version.startswith("0.3."):
+                checkout(curr_version)
+                cargo_run(["clone", url])
+                os.chdir(base)
 
-            if clone_result["returncode"] != 0:
-                errors.append(f"""
-#####################
-### path: command ###
-version: {old_version}
-{os.getcwd()}: rag clone {url}
+                # 0.4.x and newer supports gemini models, but 0.3.x does not
+                # so I'll just ignore the models
+                cargo_run(["model", "--remove", "--all"])
 
-### returncode ###
+                # `rag clone` is not compatible, but `rag archive-create` must be available in 0.3.x
+                checkout(old_version)
+                cargo_run(["archive-create", "-o", "../ar"])
+                os.chdir("..")
+                shutil.rmtree(base)
+                cargo_run(["archive-extract", "ar", "-o", base])
+                os.remove("ar")
 
-{clone_result["returncode"]}
-
-### stdout ###
-
-{clone_result["stdout"]}
-
-### stderr ###
-
-{clone_result["stderr"]}"""
-                )
-                continue
+            else:
+                clone_result = cargo_run(["clone", url])
 
         checkout(curr_version)
 
@@ -50,6 +47,3 @@ version: {old_version}
                 cargo_run(["check"])
                 os.chdir("..")
                 shutil.rmtree(base)
-
-    if errors != []:
-        raise Exception("\n\n".join(errors))
