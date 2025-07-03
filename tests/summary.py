stanley-fork/ragit

commit c32f0f614c181597804d1f5751f422b77636d9d5

draft `rag summary`

src/index/commands/summary.rs
@@ -1,0 +1,210 @@
+use crate::agent::{
+    Action as AgentAction,
+    FileTree,
+};
+use crate::error::Error;
+use crate::index::Index;
+use crate::uid::Uid;
+use ragit_fs::extension;
+use serde::{Deserialize, Serialize};
+use std::collections::{HashMap, HashSet};
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Summary {
+    /// Uid of the knowledge-base *before* this summary is added.
+    /// Adding a summary to the knowledge-base alters the uid of the base,
+    /// but it's okay because we can get the previous uid by subtracting
+    /// the uid of the summary.
+    pub uid: Uid,
+    pub summary: String,
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum SummaryMode {
+    Force,
+    Cached,
+}
+
+impl SummaryMode {
+    pub fn parse_flag(flag: &str) -> Option<Self> {
+        match flag {
+            "--force" => Some(SummaryMode::Force),
+            "--cached" => Some(SummaryMode::Cached),
+            _ => None,
+        }
+    }
+}
+
+impl Index {
+    pub async fn summary(
+        &mut self,
+        mode: Option<SummaryMode>,
+    ) -> Result<Option<String>, Error> {
+        match mode {
+            Some(SummaryMode::Force) => {},
+            Some(SummaryMode::Cached)
+            | None => {
+                self.calculate_and_save_uid()?;
+
+                match self.get_summary() {
+                    Some(summary) => {
+                        return Ok(Some(summary.to_string()));
+                    },
+                    None if mode == Some(SummaryMode::Cached) => {
+                        return Ok(None);
+                    },
+                    _ => {},
+                }
+            },
+        }
+
+        let mut actions = AgentAction::all_actions();
+
+        // If we don't filter this out, the AI is likely to copy-paste the previous version of summary.
+        actions = actions.into_iter().filter(
+            |action| *action != AgentAction::GetSummary
+        ).collect();
+
+        let summary = self.agent(
+            "Give me a summary of the knowledge-base.",
+            true,  // single paragraph
+            self.get_rough_summary()?,  // initial context
+            actions,
+        ).await?;
+
+        // We have to make sure that `self.calculate_uid` calculates uid
+        // without any summary.
+        self.summary = None;
+        self.uid = None;
+
+        self.summary = Some(Summary {
+            uid: self.calculate_uid()?,
+            summary,
+        });
+        self.uid = None;
+
+        // Now it'll update the uid with the summary
+        self.calculate_and_save_uid()?;
+        Ok(Some(self.summary.clone().unwrap().summary))
+    }
+
+    pub fn set_summary(&mut self, summary: &str) -> Result<(), Error> {
+        // We have to make sure that `self.calculate_uid` calculates uid
+        // without any summary.
+        self.summary = None;
+        self.uid = None;
+
+        self.summary = Some(Summary {
+            uid: self.calculate_uid()?,
+            summary: summary.to_string(),
+        });
+        self.uid = None;
+
+        // Now it'll update the uid with the summary
+        self.calculate_and_save_uid()?;
+        Ok(())
+    }
+
+    /// This is a read-only version of `Index::summary`.
+    /// It returns summary only if there's both summary
+    /// and uid and the summary is up-to-date.
+    pub fn get_summary(&self) -> Option<&str> {
+        match (&self.summary, self.uid) {
+            (Some(summary), Some(uid)) => {
+                // `Summary` has a uid of the knowledge-base without the summary.
+                // So if we add the uid of the summary to it, we must get the
+                // current uid of the knowledge-base. Otherwise, the knowledge-base
+                // has been edited and the summary is not valid anymore.
+                let summary_uid = Uid::new_summary(&summary.summary);
+
+                // TODO: what if `summary_uid` has exactly 0xffff_ffff uids?
+                if (summary_uid + summary.uid).clear_metadata() == uid.clear_metadata()
+                && summary_uid.get_data_size() + 1 == uid.get_data_size() {
+                    Some(&summary.summary)
+                }
+
+                else {
+                    None
+                }
+            },
+            _ => None,
+        }
+    }
+
+    pub fn remove_summary(&mut self) -> Result<(), Error> {
+        if self.summary.is_some() {
+            self.summary = None;
+            self.uid = None;
+            self.calculate_and_save_uid()?;
+        }
+
+        Ok(())
+    }
+
+    // This is an initial context of the summary agent.
+    pub(crate) fn get_rough_summary(&self) -> Result<String, Error> {
+        // HashMap<extension, number of chunks>
+        // It counts chunks instead of files because files have variable lengths,
+        // but chunks have a length limit.
+        let mut count_by_extension = HashMap::new();
+
+        // It's not always equal to `self.chunk_count` because some chunks are not
+        // from a file.
+        let mut file_chunk_count = 0;
+
+        let mut file_tree = FileTree::root();
+        let mut char_len = 0;
+        let mut image_uids = HashSet::new();
+
+        for (file, uid) in self.processed_files.iter() {
+            let chunks = self.get_chunks_of_file(*uid)?;
+            let extension = extension(file)?
+                .map(|e| format!(".{e}"))
+                .unwrap_or_else(|| String::from("no extension"));
+            file_tree.insert(file);
+            file_chunk_count += chunks.len();
+
+            match count_by_extension.get_mut(&extension) {
+                Some(n) => { *n += chunks.len() },
+                _ => { count_by_extension.insert(extension, chunks.len()); },
+            }
+
+            for chunk in chunks.iter() {
+                let chunk = self.get_chunk_by_uid(*chunk)?;
+                char_len += chunk.char_len;
+
+                for image in chunk.images.iter() {
+                    image_uids.insert(*image);
+                }
+            }
+        }
+
+        let mut count_by_extension = count_by_extension.into_iter().collect::<Vec<_>>();
+        count_by_extension.sort_by_key(|(_, count)| *count);
+
+        let common_extensions = match count_by_extension.len() {
+            0 => String::from(""),
+            1 => format!("All the files have the same extension: `{}`", count_by_extension[0].0),
+            2 => format!(
+                "The files' extension is either `{}` ({:.3} %) or `{}` ({:.3} %)",
+                count_by_extension[0].0, count_by_extension[0].1 as f64 * 100.0 / file_chunk_count as f64,
+                count_by_extension[1].0, count_by_extension[1].1 as f64 * 100.0 / file_chunk_count as f64,
+            ),
+            _ => format!(
+                "The most common extensions of the files are `{}` ({:.3} %), `{}` ({:.3} %) and `{}` ({:.3} %)",
+                count_by_extension[0].0, count_by_extension[0].1 as f64 * 100.0 / file_chunk_count as f64,
+                count_by_extension[1].0, count_by_extension[1].1 as f64 * 100.0 / file_chunk_count as f64,
+                count_by_extension[2].0, count_by_extension[2].1 as f64 * 100.0 / file_chunk_count as f64,
+            ),
+        };
+
+        Ok(format!(
+            "This knowledge-base consists of {} files ({} characters of text and {} images). {}\nBelow is the list of the files and directories in the knowledge-base.\n\n{}",
+            self.processed_files.len(),
+            char_len,
+            image_uids.len(),
+            common_extensions,
+            file_tree.render(),
+        ))
+    }
+}

tests/summary.py
@@ -1,0 +1,66 @@
+from utils import (
+    cargo_run,
+    goto_root,
+    mk_and_cd_tmp_dir,
+    rand_word,
+    write_string,
+)
+
+# Ragit commands are not supposed to fail however stupid AIs are.
+# So it'll generate a summary even with the dummy model.
+def summary():
+    goto_root()
+    mk_and_cd_tmp_dir()
+
+    cargo_run(["init"])
+    cargo_run(["config", "--set", "model", "dummy"])
+    uid_empty = get_uid()
+
+    # I wish `rag summary` works with an empty knowledge-base
+    cargo_run(["summary"])
+    assert uid_empty != get_uid()
+
+    write_string("a.txt", "Hello, World!")
+    cargo_run(["add", "a.txt"])
+    cargo_run(["build"])
+
+    # Now that the knowledge-base was edited, the summary has to be invalidated
+    assert cargo_run(["summary", "--cached"], check=False) != 0
+
+    uid_without_summary = get_uid()
+
+    # `summary --remove` should be nop because the summary has already been invalidated
+    cargo_run(["summary", "--remove"])
+    assert uid_without_summary == get_uid()
+
+    # create a summary with the dummy model
+    cargo_run(["summary"])
+    assert uid_without_summary != get_uid()
+    uid_with_dummy_summary = get_uid()
+    dummy_summary = get_summary()
+
+    rand_summary = rand_word()
+    cargo_run(["summary", "--set", rand_summary])
+    assert uid_with_dummy_summary != get_uid()
+    assert dummy_summary != get_summary()
+
+    uid_with_rand_summary = get_uid()
+    assert rand_summary == get_summary()
+
+    # it'll reuse the cached summary if there's no `--force` option
+    assert uid_with_rand_summary == get_uid()
+    assert rand_summary == get_summary()
+
+    cargo_run(["summary", "--force"])
+    assert uid_with_rand_summary != get_uid()
+    assert rand_summary != get_summary()
+
+    cargo_run(["summary", "--remove"])
+    assert uid_without_summary == get_uid()
+    assert cargo_run(["summary", "--cached"], check=False) != 0
+
+def get_uid():
+    return cargo_run(["uid"], stdout=True).strip()
+
+def get_summary():
+    return cargo_run(["summary"], stdout=True).strip()

src/agent.rs
@@ -4,7 +4,6 @@ use crate::index::Index;
 use ragit_api::Request;
 use ragit_fs::{
     WriteMode,
-    extension,
     join,
     write_string,
 };
@@ -16,7 +15,7 @@ use ragit_pdl::{
 };
 use serde::Serialize;
 use serde_json::Value;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 mod action;
 mod file_tree;
@@ -195,6 +194,12 @@ impl ActionTrace {
             let action = self.actions[n - 1];  // AI uses 1-based index
             self.index = Some(n);
             self.instruction = Some(action.get_instruction());
+
+            if !action.requires_argument() {
+                // See comments in `Action::get_instruction`
+                self.argument = Some(String::from("okay"));
+                self.result = Some(action.run("", index).await?);
+            }
         }
 
         else if self.argument.is_none() {
@@ -235,6 +240,13 @@ impl Index {
     ) -> Result<String, Error> {
         // dedup
         actions = actions.into_iter().collect::<HashSet<_>>().into_iter().collect();
+
+        // It cannot get summary if there's no summary.
+        if self.get_summary().is_none() {
+            actions = actions.into_iter().filter(
+                |action| *action != Action::GetSummary
+            ).collect();
+        }
 
         let mut state = AgentState::default();
         state.single_paragraph = single_paragraph;
@@ -314,72 +326,5 @@ impl Index {
         }
 
         Ok(state)
-    }
-
-    /// This is an initial context of a summary agent.
-    pub fn get_rough_summary(&self) -> Result<String, Error> {
-        // HashMap<extension, number of chunks>
-        // It counts chunks instead of files because files have variable lengths,
-        // but chunks have a length limit.
-        let mut count_by_extension = HashMap::new();
-
-        // It's not always equal to `self.chunk_count` because some chunks are not
-        // from a file.
-        let mut file_chunk_count = 0;
-
-        let mut file_tree = FileTree::root();
-        let mut char_len = 0;
-        let mut image_uids = HashSet::new();
-
-        for (file, uid) in self.processed_files.iter() {
-            let chunks = self.get_chunks_of_file(*uid)?;
-            let extension = extension(file)?
-                .map(|e| format!(".{e}"))
-                .unwrap_or_else(|| String::from("no extension"));
-            file_tree.insert(file);
-            file_chunk_count += chunks.len();
-
-            match count_by_extension.get_mut(&extension) {
-                Some(n) => { *n += chunks.len() },
-                _ => { count_by_extension.insert(extension, chunks.len()); },
-            }
-
-            for chunk in chunks.iter() {
-                let chunk = self.get_chunk_by_uid(*chunk)?;
-                char_len += chunk.char_len;
-
-                for image in chunk.images.iter() {
-                    image_uids.insert(*image);
-                }
-            }
-        }
-
-        let mut count_by_extension = count_by_extension.into_iter().collect::<Vec<_>>();
-        count_by_extension.sort_by_key(|(_, count)| *count);
-
-        let common_extensions = match count_by_extension.len() {
-            0 => String::from(""),
-            1 => format!("All the files have the same extension: `{}`", count_by_extension[0].0),
-            2 => format!(
-                "The files' extension is either `{}` ({:.3} %) or `{}` ({:.3} %)",
-                count_by_extension[0].0, count_by_extension[0].1 as f64 * 100.0 / file_chunk_count as f64,
-                count_by_extension[1].0, count_by_extension[1].1 as f64 * 100.0 / file_chunk_count as f64,
-            ),
-            _ => format!(
-                "The most common extensions of the files are `{}` ({:.3} %), `{}` ({:.3} %) and `{}` ({:.3} %)",
-                count_by_extension[0].0, count_by_extension[0].1 as f64 * 100.0 / file_chunk_count as f64,
-                count_by_extension[1].0, count_by_extension[1].1 as f64 * 100.0 / file_chunk_count as f64,
-                count_by_extension[2].0, count_by_extension[2].1 as f64 * 100.0 / file_chunk_count as f64,
-            ),
-        };
-
-        Ok(format!(
-            "This knowledge-base consists of {} files ({} characters of text and {} images). {}\nBelow is the list of the files and directories in the knowledge-base.\n\n{}",
-            self.processed_files.len(),
-            char_len,
-            image_uids.len(),
-            common_extensions,
-            file_tree.render(),
-        ))
     }
 }

src/agent/action.rs
@@ -12,6 +12,11 @@ pub enum Action {
     SearchExact,
     SearchTfidf,
 
+    /// This action will be filtered out if there's no summary.
+    /// Please make sure to run `rag summary` if you want to
+    /// use this action.
+    GetSummary,
+
     /// I'm using the term "simple" because I want to make sure that it's not an agentic RAG.
     SimpleRag,
 }
@@ -23,18 +28,33 @@ impl Action {
             Action::ReadDir,
             Action::SearchExact,
             Action::SearchTfidf,
+            Action::GetSummary,
             Action::SimpleRag,
         ]
     }
 
+    // If this action requires an argument, the instruction must be "Give me an argument. The argument must be...".
+    // If it doesn't require an argument, an AI will always reply "okay" to the instruction (I'll push a fake turn).
     pub(crate) fn get_instruction(&self) -> String {
         match self {
             Action::ReadFile => "Give me an exact path of a file that you want to read. Don't say anything other than the path of the file.",
             Action::ReadDir => "Give me an exact path of a directory that you want to read. Don't say anything other than the path of the directory.",
             Action::SearchExact => "Give me a keyword that you want to search for. It's not a pattern, just a keyword (case-sensitive). I'll use exact-text-matching to search. Don't say anything other than the keyword.",
             Action::SearchTfidf => "Give me a comma-separated list of keywords that you want to search for. Don't say anything other than the keywords.",
+            Action::GetSummary => "I'll give you the summary. Hold on.",
             Action::SimpleRag => "Give me a simple factual question. Don't say anything other than the question.",
         }.to_string()
+    }
+
+    pub(crate) fn requires_argument(&self) -> bool {
+        match self {
+            Action::ReadFile => true,
+            Action::ReadDir => true,
+            Action::SearchExact => true,
+            Action::SearchTfidf => true,
+            Action::GetSummary => false,
+            Action::SimpleRag => true,
+        }
     }
 
     pub(crate) fn write_prompt(actions: &[Action]) -> String {
@@ -49,6 +69,7 @@ impl Action {
             Action::ReadDir => "See a list of files in a directory: if you give me the exact path of a directory, I'll show you a list of the files in the directory.",
             Action::SearchExact => "Search by a keyword (exact): if you give me a keyword, I'll give you a list of files that contain the exact keyword in their contents.",
             Action::SearchTfidf => "Search by keywords (tfidf): if you give me keywords, I'll give you a tfidf search result. It tries to search for files that contain any of the keywords, even though there's no exact match.",
+            Action::GetSummary => "Get summary of the entire knowledge-base.",
             Action::SimpleRag => "Call a simple RAG agent: if you ask a simple factual question, a RAG agent will read the files and answer your question. You can only ask a simple factual question, not complex reasoning questions.",
         }.to_string()
     }
@@ -198,6 +219,11 @@ impl Action {
                         ).collect::<Vec<_>>().join("\n\n")
                     )
                 }
+            },
+            Action::GetSummary => {
+                // The summary must exist. Otherwise, this action should have been filtered out.
+                let summary = index.get_summary().unwrap();
+                summary.to_string()
             },
             Action::SimpleRag => {
                 let response = index.query(

src/error.rs
@@ -23,6 +23,7 @@ pub enum Error {
     NoFileToRemove,
     NoRemoteToPullFrom,
     NoSuchMeta(String),
+    NoSummary,
     CorruptedFile { path: Path, message: Option<String> },
     CliError {
         message: String,

src/index.rs
@@ -77,6 +77,8 @@ pub use commands::{
     PushResult,
     RecoverResult,
     RemoveResult,
+    Summary,
+    SummaryMode,
     VersionInfo,
     get_compatibility_warning,
 };
@@ -113,6 +115,7 @@ pub struct Index {
     pub ii_status: IIStatus,
 
     pub uid: Option<Uid>,
+    pub summary: Option<Summary>,
 
     #[serde(skip)]
     pub root_dir: Path,
@@ -159,6 +162,7 @@ impl Index {
             repo_url: None,
             ii_status: IIStatus::None,
             uid: None,
+            summary: None,
             root_dir: String::from("."),
             build_config: BuildConfig::default(),
             query_config: QueryConfig::default(),
@@ -214,6 +218,7 @@ impl Index {
             repo_url: None,
             ii_status: IIStatus::None,
             uid: None,
+            summary: None,
             prompts: PROMPTS.clone(),
             models: vec![],
         };
@@ -243,6 +248,7 @@ impl Index {
             repo_url: None,
             ii_status: IIStatus::None,
             uid: None,
+            summary: None,
             prompts: PROMPTS.clone(),
             models: vec![],
         };

src/index/commands.rs
@@ -21,6 +21,7 @@ mod pull;
 mod push;
 mod recover;
 mod remove;
+mod summary;
 mod uid;
 
 pub use add::{AddMode, AddResult};
@@ -32,6 +33,7 @@ pub use pull::PullResult;
 pub use push::PushResult;
 pub use recover::RecoverResult;
 pub use remove::RemoveResult;
+pub use summary::{Summary, SummaryMode};
 
 pub fn erase_lines(n: usize) {
     if n != 0 {

src/index/commands/merge.rs
@@ -299,6 +299,7 @@ impl Index {
         }
 
         if !dry_run {
+            // FIXME: why does it save to file twice? is it intentional?
             self.reset_uid(true /* save_to_file */)?;
             self.save_to_file()?;
         }

commit 430dc0cbca79bba2feeef9eedd3836f92947a707

fix `tests/summary.py`
This test has been failing since c32f0f614. I thought it has something
to do with `rag uid`, so added an extra check at 93796efed, but I
realized that there was in error at `Index::summary()`.

src/index/commands/summary.rs
@@ -113,11 +113,12 @@ impl Index {
                 // So if we add the uid of the summary to it, we must get the
                 // current uid of the knowledge-base. Otherwise, the knowledge-base
                 // has been edited and the summary is not valid anymore.
+                let uid_without_summary = summary.uid;
                 let summary_uid = Uid::new_summary(&summary.summary);
 
-                // TODO: what if `summary_uid` has exactly 0xffff_ffff uids?
-                if (summary_uid + summary.uid).clear_metadata() == uid.clear_metadata()
-                && summary_uid.get_data_size() + 1 == uid.get_data_size() {
+                // TODO: what if `uid_without_summary` has exactly 0xffff_ffff uids?
+                if (summary_uid + uid_without_summary).clear_metadata() == uid.clear_metadata()
+                && uid_without_summary.get_data_size() + 1 == uid.get_data_size() {
                     Some(&summary.summary)
                 }
 

tests/summary.py
@@ -18,6 +18,7 @@ def summary():
 
     # I wish `rag summary` works with an empty knowledge-base
     cargo_run(["summary"])
+    cargo_run(["check"])
     assert uid_empty != get_uid()
 
     write_string("a.txt", "Hello, World!")
@@ -26,21 +27,25 @@ def summary():
 
     # Now that the knowledge-base was edited, the summary has to be invalidated
     assert cargo_run(["summary", "--cached"], check=False) != 0
+    cargo_run(["check"])
 
     uid_without_summary = get_uid()
 
     # `summary --remove` should be nop because the summary has already been invalidated
     cargo_run(["summary", "--remove"])
+    cargo_run(["check"])
     assert uid_without_summary == get_uid()
 
     # create a summary with the dummy model
     cargo_run(["summary"])
+    cargo_run(["check"])
     assert uid_without_summary != get_uid()
     uid_with_dummy_summary = get_uid()
     dummy_summary = get_summary()
 
     rand_summary = rand_word()
     cargo_run(["summary", "--set", rand_summary])
+    cargo_run(["check"])
     assert uid_with_dummy_summary != get_uid()
     assert dummy_summary != get_summary()
 
@@ -52,10 +57,12 @@ def summary():
     assert rand_summary == get_summary()
 
     cargo_run(["summary", "--force"])
+    cargo_run(["check"])
     assert uid_with_rand_summary != get_uid()
     assert rand_summary != get_summary()
 
     cargo_run(["summary", "--remove"])
+    cargo_run(["check"])
     assert uid_without_summary == get_uid()
     assert cargo_run(["summary", "--cached"], check=False) != 0
 