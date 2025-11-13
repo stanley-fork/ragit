b091d334932387d440bf47b90db4bd361d57a578
baehyunsol <baehyunsol@naver.com>
2025-11-13 23:41:54 +0900

    update `tests/retrieve_chunks.py`
    More tests!

--- tests/retrieve_chunks.py
+++ tests/retrieve_chunks.py
@@ -28,4 +28,16 @@ def retrieve_chunks(test_model: str):
     # It's really tough to check whether the result is good or bad.
     # Let's just make sure that it doesn't panic, and dump the result to `result.json`.
     r = cargo_run(["retrieve-chunks", f"--model={test_model}", question, "--super-rerank"], stdout=True)
-    send_message(f"--- {question} ---\n\n{r}")
+    send_message(f"--- {question} (--super-rerank) ---\n\n{r}")
+
+    # I just got curious how effective `--super-rerank` is.
+    os.chdir("..")
+    cargo_run(["clone", "https://ragit.baehyunsol.com/sample/rustc"])
+    os.chdir("rustc")
+    question = "How does the rust compiler implement type inference? I also wanna know whether it has subtyping."
+
+    without_super_rerank = cargo_run(["retrieve-chunks", question, f"--model={test_model}", "--max-retrieval=5"], stdout=True)
+    with_super_rerank = cargo_run(["retrieve-chunks", question, f"--model={test_model}", "--max-retrieval=5", "--super-rerank"], stdout=True)
+
+    send_message(f"--- {question} ---\n\n{without_super_rerank}")
+    send_message(f"--- {question} (--super-rerank) ---\n\n{with_super_rerank}")
